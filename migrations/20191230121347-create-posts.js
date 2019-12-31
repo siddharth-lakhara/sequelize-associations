d4a2f3ba9413b4a5ecd2e2855065b3ac1583d52a
lakhara <siddharth.lakhara@gmail.com>
2019-12-31 12:03:05 +0530

    oneToMany, 1:n

---
+++ migrations/20191230121347-create-posts.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('posts', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      references: { model: 'users', key: 'id' },
+      onDelete: 'CASCADE',
+    },
+    title: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('posts'),
+};
